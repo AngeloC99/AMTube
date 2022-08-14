6fccefa7f53a6cec87b22055d7c0f1a3604f6d94
Angelo <angelocasciani@live.it>
2022-08-14 11:02:40 +0200

    Added first version of ApiGateway

---
+++ AMTube/ApiGateway/src/main/java/it/uniroma1/ApiGateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package it.uniroma1.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+
+@SpringBootApplication
+@EnableRetry
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
