303113ac925fc5fed1a11083fec5b120b6a36ad5
MarcoDL99 <marcodl722@gmail.com>
2022-08-23 15:53:53 +0200

    Added Font-Awesome CSS and Search Bar to header

--- AMTube/Frontend/src/app/pages/home/home.component.ts
+++ AMTube/Frontend/src/app/pages/home/home.component.ts
@@ -3,14 +3,14 @@ import {VideoService} from "../../services/video.service";
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
 import {USER_ID} from "../../constants";
-
+import { Video } from 'src/app/model/video.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  videos = [1,2,3,4,5,6,7,8]
   constructor(private videoService: VideoService, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
